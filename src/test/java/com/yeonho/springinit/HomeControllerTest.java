40283057363be292062de5cf7458502f9a1dae55
hrlee <hrlee@bandisnc.com>
2020-11-02 20:06:28 +0900

    spring-security-1

---
+++ src/test/java/com/yeonho/springinit/HomeControllerTest.java
@@ -0,0 +1,40 @@
+package com.yeonho.springinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void hello () throws Exception {
+        mockMvc.perform(get("/hello")
+                .accept(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    @WithMockUser
+    public void my () throws Exception {
+        mockMvc.perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+
+}
